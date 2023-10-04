bbf26a4c6ba78ac9d3c93dc279136b242d33e0be
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-10-04 15:06:22 +0200

    Data handling and storage finished

--- src/experiments/sqlalchemy/main.py
+++ src/experiments/sqlalchemy/main.py
@@ -10,13 +10,13 @@ engine = create_engine('sqlite://', echo=False)
 BaseModel.metadata.create_all(engine)
 
 with Session(engine) as session:
-    builder = DatasetFilePathBuilder(path='/Users/ralph/Desktop/downloads/dataset')
+    builder = DatasetFilePathBuilder(
+        path='/Users/ralph/Desktop/downloads/dataset',
+        name='myDataset',
+    )
     dataset = builder.execute()
-
     manager = DatasetStorageManager(session=session)
     name = manager.save(dataset)
-
     dataset = manager.load(name)
     print(dataset)
-
     manager.delete(name)
