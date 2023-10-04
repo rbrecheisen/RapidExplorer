4829809e863519584b04f25d98614811ab528e94
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-10-04 23:22:48 +0200

    Added list widget to datasets dock widget

--- src/app2/widgets/datasetdockwidget.py
+++ src/app2/widgets/datasetdockwidget.py
@@ -1,11 +1,21 @@
-from PySide6.QtWidgets import QDockWidget, QTextEdit
+from PySide6.QtWidgets import QDockWidget, QListWidget, QListWidgetItem, QSizePolicy
 from widgets.dockwidget import Dockwidget
 
 
 class DatasetDockWidget(Dockwidget):
     def __init__(self, title: str, minWidth: int, maxWidth) -> None:
         super(DatasetDockWidget, self).__init__(title)
-        self.setMinimumWidth(minWidth)
-        self.setMaximumHeight(maxWidth)
-        self.setWidget(QTextEdit('Place holder for datasets'))
+        self.minWidth = minWidth
+        self.maxWidth = maxWidth
+        self.initUi()
+
+    def initUi(self) -> None:
+        listWidget = QListWidget()
+        listWidget.addItem(QListWidgetItem('Item 1'))
+        listWidget.addItem(QListWidgetItem('Item 2'))
+        listWidget.addItem(QListWidgetItem('Item 3'))
+        listWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.setWidget(listWidget)
+        self.setMinimumWidth(self.minWidth)
+        self.setMaximumHeight(self.maxWidth)
         self.setFeatures(QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
