09d2fe1f0b95abe47b4988a2d4c3af903ed89d12
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-10-16 17:13:50 +0200

    Saving work

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rm -rf distribution
-# consider using --disable-console but also requires --macos-create-app-bundle
-# try this on windows as well
+# TODO:
+# (1) Consider using --disable-console but also requires --macos-create-app-bundle
+# (2) Try this on windows as well
 ~/.venv/rapid/bin/python -m nuitka --enable-plugin=pyside6 --standalone src/app/main.py
-mv main.dist/main.bin main.dist/rapid-x
+mv main.dist/main.bin main.dist/rapidx
 mv main.dist distribution
 rm -rf main.build
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./distribution/rapid-x
+./distribution/rapidx
