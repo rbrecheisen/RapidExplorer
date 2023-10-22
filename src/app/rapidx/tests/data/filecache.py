f78f8aade5c9e6a941d3ef0ce25389d232def2d7
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-10-22 22:43:10 +0200

    Saving work

--- src/app/rapidx/tests/data/filecache.py
+++ src/app/rapidx/tests/data/filecache.py
@@ -19,11 +19,20 @@ class FileCache:
         multiFileSetModel = MultiFileSetModelFactory.create()
         fileSetModel = FileSetModelFactory.create(..., multiFileSetModel=multiFileSetModel)
         fileModel = FileModelFactory.create(..., fileSetModel=fileSetModel)
-        dicomFile = DicomFile(fileModel)
+        dicomFile = DicomFile(fileModel)        # Generate a UUID here or in FileModel that we can use to lookup?
         cache = FileCache()
         cache.add(file=dicomFile, model=multiFileSetModel)
-        
 
+    When the application starts up and database contains a registered MultiFileSetModel with corresponding FileSetModel
+    and FileModel, this data should be loaded into the tree widget. At that point there is no physical data like a 
+    DICOM file in the file cache (it was emptied when the application closed). 
+
+    When you manually import a DICOM file, you're using the DicomFileImporter or DicomFileSetImporter. It will create
+    a new DicomFile that can be stored in the file cache. However, when you only have the SQL database and the file
+    cache is empty, how do you reload the physical DICOM file? Use a loader instead of an importer? What do we provide
+    to the loader? DicomFileLoader(fileModel=fileModel)? When you click a file in the tree widget, this should trigger
+    a load action on the file (which is a DICOM file). How do you know to use a DicomFileLoader and not something else?
+    Like a PngFileLoader? This should be stored in SQL as well I would say, perhaps with a flag or enum field?
     """
     def __init__(self) -> None:
         self._data = {}
