b0121aba821d740e57363a1b99a680defae7823f
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-11-05 08:42:15 +0100

    Saving work

--- compile.sh
+++ compile.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+#Clean up left overs of previous builds
 rm -rf ./RapidX
 
-cd src/app/rapidx
-~/.venv/rapidx/bin/pyside6-rcc -o resources.py resources.qrc
+# Compile Qt resources (if any)
+~/.venv/rapidx/bin/pyside6-rcc -o src/app/rapidx/resources.py src/app/rapidx/resources.qrc
 
-cd ../../../
-~/.venv/rapidx/bin/python -m nuitka --include-package=pydicom --enable-plugin=pyside6 --standalone src/app/main.py
+# Build executable. This is the same command on MacOS or Windows. If you want to disable the console
+# use the flag --disable-console on MacOS or --windows-disable-console on Windows
+~/.venv/rapidx/bin/python -m nuitka --standalone --include-package=pydicom --enable-plugin=pyside6 src/app/main.py
 
+# Reorganize
 mv main.dist/main.bin main.dist/RapidX
 mv main.dist RapidX
 
+# Build a ZIP file for the application's distribution
+# TODO: Add a README explaining how to get it running if errors about signing occur?
 zip -r RapiX.zip RapidX
 
+# Clean up
 rm -rf main.build RapidX
 
+# Build a MacOS app (not going to do that, too complicated and expensive)
 # mkdir RapidX.app
 # mkdir RapidX.app/Contents
 # mkdir RapidX.app/Contents/MacOS
