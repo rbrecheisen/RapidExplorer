ebd908e2f98591697f89e96959013aaf0f85b7f2
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-11-24 13:14:43 +0100

    Saving work

--- compile.sh
+++ compile.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+export NUITKA_CCACHE_BINARY=none
+
 # Compile Qt resources (if any)
 ~/.venv/RapidExplorer/bin/pyside6-rcc -o src/app/resources.py src/app/resources.qrc
 
 # Build executable. This is the same command on MacOS or Windows. If you want to disable the console
 # use the flag --disable-console on MacOS or --windows-disable-console on Windows. For MacOS or 
 # Windows you do need to create different startup scripts
-~/.venv/RapidExplorer/bin/python -m nuitka --standalone --include-package=pydicom --enable-plugin=pyside6 src/app/main.py
+~/.venv/RapidExplorer/bin/python -m nuitka \
+    --standalone \
+    --include-data-dir=*/PythonModules \
+    --include-package=pydicom \
+    --enable-plugin=pyside6 \
+    src/app/main.py
 
 # Reorganize
 mv main.dist/main.bin main.dist/RapidExplorer
