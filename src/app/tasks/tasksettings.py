df2917c26b168030ea6ed4ba4f43643d65650aac
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-11-30 14:09:36 +0100

    Saving work

--- src/app/tasks/tasksettings.py
+++ src/app/tasks/tasksettings.py
@@ -1,17 +1,41 @@
 from typing import List
 
 from tasks.tasksetting import TaskSetting
+from tasks.tasksettingboolean import TaskSettingBoolean
+from tasks.tasksettingfileset import TaskSettingFileSet
+from tasks.tasksettingfloatingpoint import TaskSettingFloatingPoint
+from tasks.tasksettinginteger import TaskSettingInteger
+from tasks.tasksettingoptionlist import TaskSettingOptionList
+from tasks.tasksettingtext import TaskSettingText
 
 
 class TaskSettings:
     def __init__(self) -> None:
         self._settings = {}
 
+    def all(self) -> List[TaskSetting]:
+        return self._settings.values()
+        
     def setting(self, name: str) -> TaskSetting:
         return self._settings[name]
     
     def add(self, setting: TaskSetting) -> None:
         self._settings[setting.name()] = setting
 
-    def settings(self) -> List[TaskSetting]:
-        return self._settings.values()
+    def isTypeBoolean(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingBoolean)
+
+    def isTypeFileSet(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingFileSet)
+
+    def isTypeFloatingPoint(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingFloatingPoint)
+
+    def isTypeInteger(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingInteger)
+
+    def isTypeOptionList(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingOptionList)
+
+    def isTypeText(self, setting: TaskSetting) -> bool:
+        return isinstance(setting, TaskSettingText)
--- src/app/widgets/tasksettingsdialog.py
+++ src/app/widgets/tasksettingsdialog.py
@@ -1,12 +1,43 @@
-from PySide6.QtWidgets import QDialog
+from PySide6.QtWidgets import QDialog, QComboBox, QSpinBox, QDoubleSpinBox, QLineEdit
 
 
 class TaskSettingsDialog(QDialog):
     def __init__(self, task: Task) -> None:
         super(TaskSettingsDialog, self).__init__()
         self._task = task
-        self._formFieldWidgets = {}
+        self._taskSettingWidgets = {}
         self.initUi()
 
     def initUi(self) -> None:
-        pass
+        self._taskSettingWidgets = self.createTaskSettingWidgets()
+
+    def createTaskSettingWiddgets(self) ->None:
+        settings = self._task.settings()
+        for setting in settings.all():
+            if settings.isTypeIsBoolean(setting):
+                widget = QComboBox(self)
+                widget.addItems([None, 'True', 'False'])
+                self._taskSettingWidgets[setting.name()] = (setting, widget)
+
+            elif settings.isTypeFileSet(setting):
+                pass
+
+            elif settings.isTypeFloatingPoint(setting):
+                widget = QDoubleSpinBox(self)
+                widget.setRange(setting.minimum(), setting.maximum())
+                
+
+            elif settings.isTypeInteger(setting):
+                widget = QSpinBox(self)
+                widget.setRange(setting.minimum(), setting.maximum())
+                self._taskSettingWidgets[setting.name()] = (setting, widget)
+
+            elif settings.isTypeOptionList(setting):
+                pass
+
+            elif settings.isTypeText(setting):
+                pass
+
+            else:
+                raise RuntimeError(f'Unknown setting {setting.name()}')
+        return self._taskSettingWidgets
