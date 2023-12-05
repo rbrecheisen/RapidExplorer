1f66070c1478695ac2fdccbce8d380891f5927a5
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-12-05 23:03:07 +0100

    Working on create archive task

--- src/app/tasks/createarchivetask/createarchivetask.py
+++ src/app/tasks/createarchivetask/createarchivetask.py
@@ -1,5 +1,8 @@
 from tasks.task import Task
 from tasks.tasksignal import TaskSignal
+from settings.settingfileset import SettingFileSet
+from settings.settingfilesetpath import SettingFileSetPath
+from settings.settingtext import SettingText
 
 
 class CreateArchiveTask(Task):
@@ -7,3 +10,16 @@ class CreateArchiveTask(Task):
 
     def __init__(self) -> None:
         super(CreateArchiveTask, self).__init__()
+        self.settings().add(SettingFileSet(name='inputFileSetName', displayName='File Set to Compress'))
+        self.settings().add(SettingFileSetPath(name='outputDirectoryPath', displayName='Output Directory Path'))
+        self.settings().add(SettingText(name='zipFileName', displayName='ZIP File Name', optional=True))
+        self._signal = TaskSignal()
+
+    def signal(self) -> TaskSignal:
+        return self._signal
+    
+    def run(self) -> FileSet:
+        inputFileSetName = self.settings().setting(name='inputFileSetName').value()
+        if inputFileSetName:
+            inputFileSet = self.dataManager().fileSetByName(name=inputFileSetName)
+            raise RuntimeError('Continue here!')
