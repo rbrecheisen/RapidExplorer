4eed9748883e01f27b83045e0c4c86bb57a22609
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-01-06 19:32:31 +0100

    Saving work

--- src/app/tasks/dummytask/dummytaskwidget.py
+++ src/app/tasks/dummytask/dummytaskwidget.py
@@ -5,9 +5,7 @@ from tasks.dummytask.dummytask import DummyTask
 class DummyTaskWidget(TaskWidget):
     def __init__(self) -> None:
         super(DummyTaskWidget, self).__init__(taskType=DummyTask)
-        # self._inputFileSet = self.addFileSetParameter(labelText='Input File Set')
-        # self._outputFileSetPath = self.addPathParameter(labelText='Output File Set Path')
-        # self._outputFileSetName = self.addTextParameter(labelText='Output File Set Name')
+        
         self.addIntegerParameter(
             name='nrIterations', 
             labelText='Nr. Iterations',
