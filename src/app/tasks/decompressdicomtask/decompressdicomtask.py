7675095eea969c44d1dd64fa87fdf57e88829ebd
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-01-12 14:37:49 +0100

    Saving work

--- src/app/tasks/decompressdicomtask/decompressdicomtask.py
+++ src/app/tasks/decompressdicomtask/decompressdicomtask.py
@@ -1,11 +1,9 @@
 import os
 import shutil
-
-from typing import List
+import pydicom
+import pydicom.errors
 
 from tasks.task import Task
-from tasks.taskworkitem import TaskWorkItem
-from tasks.decompressdicomtask.decompressdicomtaskworkitem import DecompressDicomTaskWorkItem
 from logger import Logger
 
 LOGGER = Logger()
@@ -14,93 +12,52 @@ LOGGER = Logger()
 class DecompressDicomTask(Task):
     def __init__(self) -> None:
         super(DecompressDicomTask, self).__init__()
-        self._outputFileSetPath = None
 
-    def prepareWorkItems(self) -> List[TaskWorkItem] | str:
+    def execute(self) -> None:
+
+        # Prepare parameters, then run task
         inputFileSetName = self.parameter('inputFileSetName').value()
-        inputFileSet = self.dataManager().fileSetByName(inputFileSetName)
-        if inputFileSet:
+        inputFileSet = manager.fileSetByName(inputFileSetName)
+        if inputFileSet is not None:
+
             outputFileSetPath = self.parameter('outputFileSetPath').value()
             outputFileSetName = self.parameter('outputFileSetName').value()
             if outputFileSetName is None:
                 outputFileSetName = self.generateTimestampForFileSetName(name=inputFileSetName)
-            outputFileSetPath = os.path.join(outputFileSetPath, outputFileSetName)
+
             overwriteOutputFileSet = self.parameter('overwriteOutputFileSet').value()
+            outputFileSetPath = os.path.join(outputFileSetPath, outputFileSetName)
+            
             if overwriteOutputFileSet:
                 if os.path.isdir(outputFileSetPath):
                     shutil.rmtree(outputFileSetPath)
             os.makedirs(outputFileSetPath, exist_ok=False)
-            workItems = []
-            for file in inputFileSet.files():
-                outputFilePath = os.path.join(outputFileSetPath, file.name())
-                workItem = DecompressDicomTaskWorkItem(inputFilePath=file.path(), outputFilePath=outputFilePath)
-                workItems.append(workItem)
-            return workItems, outputFileSetPath
+            
+            step = 0
+            files = inputFileSet.files()
+            nrSteps = len(files)
+            for file in files:
+
+                # Chec if the task should cancel
+                if self.statusIsCanceled():
+                    self.addInfo('Canceling task...')
+                    break
+                
+                try:
+                    p = pydicom.dcmread(file.path())
+                    p.decompress()
+                    outputFileName = file.name()
+                    outputFilePath = os.path.join(outputFileSetPath, outputFileName)
+                    p.save_as(outputFilePath)
+                except pydicom.errors.InvalidDicomError:
+                    self.addWarning(f'Skipping non-DICOM: {file.path()}')
+
+                # Update progress for this iteration         
+                self.updateProgress(step=step, nrSteps=nrSteps)
+                step += 1
+
+            # Finalize task
+            manager.createFileSet(fileSetPath=outputFileSetPath)
+            self.addInfo('Finished')
         else:
             self.addError(f'Input fileset {inputFileSetName} does not exist')
-        return None, None
-
-    def finalize(self) -> None:
-        pass
-
-    # def run(self) -> None:
-    #     canceled = False
-    #     manager = DataManager()
-
-    #     # Prepare parameters, then run task
-    #     inputFileSetName = self.parameter('inputFileSetName').value()
-    #     inputFileSet = manager.fileSetByName(inputFileSetName)
-    #     if inputFileSet is not None:
-    #         outputFileSetPath = self.parameter('outputFileSetPath').value()
-    #         outputFileSetName = self.parameter('outputFileSetName').value()
-    #         if outputFileSetName is None:
-    #             outputFileSetName = self.generateTimestampForFileSetName(name=inputFileSetName)
-    #         overwriteOutputFileSet = self.parameter('overwriteOutputFileSet').value()
-
-    #         # Already create output fileset directory here because we're writing new 
-    #         # files inside the loop
-    #         outputFileSetPath = os.path.join(outputFileSetPath, outputFileSetName)
-    #         if overwriteOutputFileSet:
-    #             if os.path.isdir(outputFileSetPath):
-    #                 shutil.rmtree(outputFileSetPath)
-    #         os.makedirs(outputFileSetPath, exist_ok=False)
-
-    #         self.addInfo(f'Running task ({self.parameterValuesAsString()})')
-    #         step = 0
-    #         files = inputFileSet.files()
-    #         nrSteps = len(files)
-    #         for file in files:
-
-    #             # Chec if the task should cancel
-    #             if self.statusIsCanceling():
-    #                 self.addInfo('Canceling task...')
-    #                 canceled = True
-    #                 break
-                
-    #             try:
-    #                 p = pydicom.dcmread(file.path())
-    #                 p.decompress()
-    #                 outputFileName = file.name()
-    #                 outputFilePath = os.path.join(outputFileSetPath, outputFileName)
-    #                 p.save_as(outputFilePath)
-    #                 # time.sleep(0.01)
-    #             except pydicom.errors.InvalidDicomError:
-    #                 self.addWarning(f'Skipping non-DICOM: {file.path()}')
-
-    #             # Update progress for this iteration         
-    #             self.updateProgress(step=step, nrSteps=nrSteps)
-    #             step += 1
-
-    #         # Finalize task
-    #         manager.createFileSet(fileSetPath=outputFileSetPath)
-    #         self.addInfo('Finished')
-    #     else:
-    #         self.addError(f'Input fileset {inputFileSetName} does not exist')
-
-    #     # Determine task final status
-    #     if canceled:
-    #         self.setStatusCanceled()
-    #     elif self.hasErrors():
-    #         self.setStatusError()
-    #     else:
-    #         self.setStatusFinished()
