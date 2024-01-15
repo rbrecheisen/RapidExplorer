e6630831be9b3bfa2fc375b12105df93463d71bd
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-01-15 13:17:28 +0100

    Working on installer scripts

--- install.sh
+++ install.sh
@@ -3,17 +3,20 @@
 # https://chat.openai.com/c/143bf330-901c-46ea-9115-03b450fdd07d
 # Also install Python3 if needed
 
-unzip -o MosamaticDesktop.zip
+echo "Unpacking source code..."
+unzip MosamaticDesktop.zip -d ./MosamaticDesktop
 
+echo "Setting up virtual environment in $HOME/.mosamatic..."
 VENV_DIR="$HOME/.mosamatic/MosamaticDesktop"
 mkdir -p $HOME/.mosamatic
-
-if [ ! -d "$VENV_DIR" ]; then
-    python3 -m venv $VENV_DIR
+if [ -d "$VENV_DIR" ]; then
+    rm -rf $HOME/.mosamatic    
 fi
+python3 -m venv $VENV_DIR
 
-source $VENV_DIR/bin/activate
-
-pip install -r requirements.txt
-
-deactivate
+echo "Activating virtual environment and installing package requirements..."
+# source $VENV_DIR/bin/activate
+$HOME/.mosamatic/MosamaticDesktop/bin/pip cache purge
+$HOME/.mosamatic/MosamaticDesktop/bin/pip install --upgrade pip
+$HOME/.mosamatic/MosamaticDesktop/bin/pip install -r requirements.txt
+# deactivate
--- run.sh
+++ run.sh
@@ -5,4 +5,8 @@ export ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export DATABASE=${ROOTDIR}/db.sqlite3
 export DATABASEECHO=0
 
-${ROOTDIR}/main
+# ${ROOTDIR}/main
+
+cd MosamaticDesktop
+echo "Running MosamaticDesktop..."
+$HOME/.mosamatic/MosamaticDesktop/bin/python main.py
