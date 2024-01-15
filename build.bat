e63b93fe02d54590ac3ed4ac1f6861344873ab4b
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-01-15 13:42:13 +0100

    Saving work

--- build.bat
+++ build.bat
@@ -21,12 +21,14 @@ FOR /F "tokens=*" %%i IN (requirements.txt) DO (
 call pyinstaller.exe main.win.spec
 @REM call pyinstaller.exe --onefile %HIDDEN_IMPORTS% --hidden-import=pydicom.encoders.gdcm --hidden-import=pydicom.encoders.pylibjpeg src\app\main.py 
 
-@REM if exist dist move /y dist %APPNAME%
-@REM copy settings.ini %APPNAME%
-@REM copy run.bat %APPNAME%
-@REM move /y %APPNAME%\run.bat %APPNAME%\%APPNAME%.bat
-@REM powershell Compress-Archive -Force -Path %APPNAME% -DestinationPath %APPNAME%.zip
-@REM if exist build rmdir /s /q build
-@REM if exist %APPNAME% rmdir /s /q %APPNAME%
+if exist dist move /y dist %APPNAME%
 
-echo "Bla"
+copy settings.ini %APPNAME%
+
+copy run.bat %APPNAME%
+move /y %APPNAME%\run.bat %APPNAME%\%APPNAME%.bat
+
+powershell Compress-Archive -Force -Path %APPNAME% -DestinationPath %APPNAME%.zip
+
+if exist build rmdir /s /q build
+if exist %APPNAME% rmdir /s /q %APPNAME%
