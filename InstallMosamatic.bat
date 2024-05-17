757f749ea682e2fb59fddd9e72a9817d90353281
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-05-17 13:53:22 +0200

    Saving version 1.40.0 before deploying to PyPI.

--- InstallMosamatic.bat
+++ InstallMosamatic.bat
@@ -4,29 +4,29 @@ setlocal EnableDelayedExpansion
 @REM https://chat.openai.com/c/143bf330-901c-46ea-9115-03b450fdd07d
 @REM Also install Python 3 if needed
 
-set VENV_DIR=%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop
+set VENV_DIR=%USERPROFILE%\Mosamatic\MosamaticDesktop
 
 echo "Creating virtual environment..."
-if not exist "%USERPROFILE%\Apps\Mosamatic" mkdir "%USERPROFILE%\Apps\Mosamatic"
-cd /d "%USERPROFILE%\Apps\Mosamatic"
+if not exist "%USERPROFILE%\.mosamatic" mkdir "%USERPROFILE%\.mosamatic"
+cd /d "%USERPROFILE%\.mosamatic"
 if not exist "%VENV_DIR%" (
     python -m venv "%VENV_DIR%"
 )
 
 echo "Installing requirements..."
 call "%VENV_DIR%\Scripts\activate"
-%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\python -m pip cache purge
-%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade pip
-%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade scikit-image
-%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade mosamaticdesktop
+%USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\python -m pip cache purge
+%USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade pip
+%USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade scikit-image
+%USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\python -m pip install --upgrade mosamaticdesktop
 call "%VENV_DIR%\Scripts\deactivate.bat"
 
 @REM echo "Installing executable..."
-@REM copy %USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\mosamatic-desktop.exe C:\Windows
+@REM copy %USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\mosamatic-desktop.exe C:\Windows
 
 echo "Creating desktop shortcut for executable..."
 @REM SET ExePath=C:\Windows\mosmatic-desktop.exe
-SET ExePath=%USERPROFILE%\Apps\Mosamatic\MosamaticDesktop\Scripts\mosamatic-desktop.exe
+SET ExePath=%USERPROFILE%\.mosamatic\MosamaticDesktop\Scripts\mosamatic-desktop.exe
 SET ShortcutName=Mosamatic Desktop
 
 FOR /F "tokens=*" %%I IN ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v Desktop') DO SET DesktopPath=%%I
