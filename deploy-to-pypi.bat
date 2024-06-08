e80dc1bed25aa3a41dd675f2539f2718340bdfe9
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-06-08 10:26:27 +0200

    Saving version 1.43.0 before deploying to PyPI.

---
+++ deploy-to-pypi.bat
@@ -0,0 +1,80 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set PACKAGE=mosamaticdesktop
+
+echo "1"
+where twine >nul 2>&1
+if %errorlevel% neq 0 (
+  echo "You do not seem to have Twine installed (wrong venv?). It is needed to upload to PyPI"
+  echo "Type 'install' to install it and continue or any other key to quit"
+  set /p line=
+  if not "%line%" == "install" (
+    exit /b 0
+  )
+  python -m pip install twine wheel
+)
+
+echo "2"
+set CMD=%1
+if "%CMD%" == "-h" (
+    echo "Usage: deploy.bat [patch^|minor^|major]"
+    exit /b 0
+)
+
+echo "3"
+if not "%CMD%" == "" if not "%CMD%" == "patch" if not "%CMD%" == "minor" if not "%CMD%" == "major" (
+    echo "Illegal argument %CMD%"
+    exit /b 1
+)
+
+echo "4"
+if "%CMD%" == "" (
+  set CMD=minor
+)
+
+echo "5"
+for /f %%i in (VERSION) do set OLD_VERSION=%%i
+
+echo "6"
+python versioning.py --major-minor=%CMD%
+copy VERSION .\mosamaticdesktop\VERSION
+for /f %%i in (VERSION) do set VERSION=%%i
+
+echo "Is this the right version? Type "yes" to continue, or any other key to quit."
+set /p line=
+if not "%line%" == "yes" (
+  exit /b 0
+)
+
+git status
+
+echo "Everything ready to be pushed to Git? Type "yes" to continue, or any other key to quit."
+set /p line=
+if not "%line%" == "yes" (
+  exit /b 0
+)
+
+echo "Type your Git commit message here below"
+set /p message=
+git add -A
+git commit -m "Saving version %VERSION% before deploying to PyPI. %message%"
+git push
+
+if %errorlevel% neq 0 (
+    echo "Something went wrong with pushing to Git. Please revert back to previous VERSION"
+    exit /b 1
+)
+
+rem GIT_COMMIT_ID=$(git log -1 --format=%%H)
+rem echo %GIT_COMMIT_ID% > GIT_COMMIT_ID
+rem copy GIT_COMMIT_ID .\mosamaticdesktop\GIT_COMMIT_ID
+
+rd /s /q build
+rd /s /q dist
+
+python setup.py sdist bdist_wheel
+
+rem twine upload --repository pypi dist/*
+for /f %%i in (C:\i\pypi-api.txt) do set PYPITOKEN=%%i
+twine upload --username "__token__" --password "%PYPITOKEN%" --repository pypi dist\*
